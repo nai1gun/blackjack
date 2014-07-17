22904e260aef985bac291cb7e0d5e75ef16a6f43
Alexander <alexander@korvyakov.com>
2014-07-17 12:49:23 +0400

    [+] javadocs

--- src/main/java/com/korvyakov/insightdataengineering/blackjack/service/stage/Stage.java
+++ src/main/java/com/korvyakov/insightdataengineering/blackjack/service/stage/Stage.java
@@ -3,17 +3,33 @@ package com.korvyakov.insightdataengineering.blackjack.service.stage;
 import com.korvyakov.insightdataengineering.blackjack.domain.Expect;
 
 /**
+ * Stage represents a state of the game
+ *
  * @author nailgun
  * @since 13.07.14
  */
 public interface Stage<T> {
 
+	/**
+	 * @return what to expect from the user after the stage
+	 */
     Expect getExpect();
 
+	/**
+	 * @return name of the corresponding template
+	 */
     String getTemplate();
 
+	/**
+	 * @return true if application should be closed after the stage
+	 */
     boolean isExit();
 
+	/**
+	 * Actions on user input
+	 * @param input command
+	 * @return the next stage
+	 */
     Stage action(T input);
 
 }
